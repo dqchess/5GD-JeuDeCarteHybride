b853b3ed29cd443ad0895006a6fb283d9d14772c
Isirda123 <isirda123@yahoo.fr>
2019-09-29 17:02:25 +0200

    Lecteur Excel + Testeur

---
+++ Assets/Lecteur_Excel/CardsInformations.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CardsInformations
+{
+    public string id;
+    public string name;
+    public string damage;
+    public string armor;
+    public string specialStat;
+}
