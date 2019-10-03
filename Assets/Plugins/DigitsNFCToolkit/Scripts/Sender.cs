6bd68964410494b079631f97bdb78ff5f47d968c
Isirda123 <isirda123@yahoo.fr>
2019-10-03 16:47:34 +0200

    TransmissionNFC_OSC | Player Settings / Parsing infos

--- Assets/Plugins/DigitsNFCToolkit/Scripts/Sender.cs
+++ Assets/Plugins/DigitsNFCToolkit/Scripts/Sender.cs
@@ -11,23 +11,49 @@ namespace DigitsNFCToolkit.Samples
         [Header("OSC Settings")]
         public OSCTransmitter Transmitter;
         public Text iDReceiveText;
+        public Phone_Player phonePlayer;
         //DigitsNFCToolkit.TextRecord textRecord;
 
         private void Start()
         {
-            //NativeNFCManager.Enable();
-            //NativeNFCManager.AddNDEFReadFinishedListener();
+
         }
 
         private void Update()
         {
-            //NativeNFCManager.AddNDEFPushFinishedListener(OnNDEFPushFinished)
-            //SendValue("YOLO");
+            SendDebug("YOLO");
         }
+        public void SendDebug(string name)
+        {
+            Send("/player", OSCValue.String(name));
+        }
+
         public void SendValue(string textToSend)
         {
-            iDReceiveText.text = textToSend;
-            Send("/joueur1", OSCValue.String(textToSend));
+
+            if (phonePlayer.player == 1)
+            {
+                iDReceiveText.text = textToSend;
+                Send("/player1/cards", OSCValue.String(textToSend));
+            }
+            else
+            {
+                iDReceiveText.text = textToSend;
+                Send("/player2/cards", OSCValue.String(textToSend));
+            }
+
+        }
+
+        public void SendPlayerName(string nameToSend)
+        {
+            if (phonePlayer.player == 1)
+            {
+                Send("/player1/name", OSCValue.String(nameToSend));
+            }
+            else
+            {
+                Send("/player2/name", OSCValue.String(nameToSend));
+            }
         }
 
         private void Send(string address, OSCValue value)
@@ -35,7 +61,6 @@ namespace DigitsNFCToolkit.Samples
             if (Transmitter != null)
             {
                 Transmitter.Send(address, value);
-                Debug.Log("ok");
             }
         }
     }
